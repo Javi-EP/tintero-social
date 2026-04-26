2e9a0e8357182a88256f6016f0f80b16105c0ed3
Javi-EP <javi.escalona@duocuc.cl>
2026-04-25 23:10:18 -0400

    feat: estructura inicial UserRepository

--- user-service/src/main/java/cl/javiep/userservice/repository/UserRepository.java
+++ user-service/src/main/java/cl/javiep/userservice/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package cl.javiep.userservice.repository;
 
-public class UserRepository {
+import cl.javiep.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    // Busca por email — usado en login y para verificar duplicados
+    Optional<User> findByEmail(String email);
+
+    // Verifica si ya existe un usuario con ese email
+    boolean existsByEmail(String email);
 }
