7c7034422327e5751ad80b6739457a5d7a38ceec
Javi-EP <javi.escalona@duocuc.cl>
2026-04-25 22:29:25 -0400

    feat: estructura inicial de LoginRequestDTO y LoginResponseDTO

--- user-service/src/main/java/cl/javiep/userservice/dto/LoginRequestDTO.java
+++ user-service/src/main/java/cl/javiep/userservice/dto/LoginRequestDTO.java
@@ -1,4 +1,21 @@
 package cl.javiep.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email no tiene un formato válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
 }
--- user-service/src/main/java/cl/javiep/userservice/dto/LoginResponseDTO.java
+++ user-service/src/main/java/cl/javiep/userservice/dto/LoginResponseDTO.java
@@ -1,4 +1,18 @@
 package cl.javiep.userservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDTO {
+    private String token;       // el token JWT
+    private String type;        // siempre "Bearer"
+    private Long userId;
+    private String name;
+    private String email;
 }
