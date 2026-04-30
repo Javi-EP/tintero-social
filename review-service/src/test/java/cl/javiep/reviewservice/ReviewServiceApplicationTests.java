9a49de22e399352600b5de874c3085bd57c02e43
Javi-EP <javi.escalona@duocuc.cl>
2026-04-29 21:57:58 -0400

    feat: creacion modulo review-service

---
+++ review-service/src/test/java/cl/javiep/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.javiep.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
