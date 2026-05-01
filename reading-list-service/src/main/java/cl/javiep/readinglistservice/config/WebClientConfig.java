2c8c39bf87a49032b8fa8d244b41d9c3e2ae2f18
Javi-EP <javi.escalona@duocuc.cl>
2026-04-30 21:44:24 -0400

    feat: adicion de dependencia WebClient y estructura inicial de WebClientConfig para llamar a otros microservicios

--- reading-list-service/src/main/java/cl/javiep/readinglistservice/config/WebClientConfig.java
+++ reading-list-service/src/main/java/cl/javiep/readinglistservice/config/WebClientConfig.java
@@ -1,4 +1,16 @@
 package cl.javiep.readinglistservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    // WebClient es el cliente HTTP de Spring para llamar a otros servicios
+    // Es como un "Postman" dentro del código
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 }
