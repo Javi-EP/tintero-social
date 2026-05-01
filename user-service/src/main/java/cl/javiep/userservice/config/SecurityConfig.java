f762e9746d3b9314dd667cdcac10a627fba76266
Javi-EP <javi.escalona@duocuc.cl>
2026-04-30 20:55:19 -0400

    feat: creación de JwtAuthFilter para poder hacer get request en postman con bearer token. Todos los endpoints están funcionando

--- user-service/src/main/java/cl/javiep/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/cl/javiep/userservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package cl.javiep.userservice.config;
 
+import cl.javiep.userservice.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,9 +8,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     // BCrypt es el algoritmo estándar para hashear contraseñas
     @Bean
@@ -27,14 +35,19 @@ public class SecurityConfig {
                                 "/api/users/register",
                                 "/api/users/login",
                                 "/api/users/validate",  // para que otros microservicios validen tokens
-                                "/h2-console/**"
+                                "/h2-console/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
                         ).permitAll()
                         // cualquier otro endpoint requiere autenticación
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.disable())  // necesario para h2-console
-                );
+                )
+
+                // Registra el filtro JWT ANTES del filtro de autenticación de Spring
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
