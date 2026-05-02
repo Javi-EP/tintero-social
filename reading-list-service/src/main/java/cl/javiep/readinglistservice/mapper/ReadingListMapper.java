f0dda14e72e476042e5ec1551535d180cdaf31d5
Javi-EP <javi.escalona@duocuc.cl>
2026-05-02 12:05:15 -0400

    feat: estructura inicial de ReadingListMapper

--- reading-list-service/src/main/java/cl/javiep/readinglistservice/mapper/ReadingListMapper.java
+++ reading-list-service/src/main/java/cl/javiep/readinglistservice/mapper/ReadingListMapper.java
@@ -1,4 +1,52 @@
 package cl.javiep.readinglistservice.mapper;
 
+import cl.javiep.readinglistservice.dto.*;
+import cl.javiep.readinglistservice.model.ReadingList;
+import cl.javiep.readinglistservice.model.ReadingListItem;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
 public class ReadingListMapper {
+
+    public ReadingList toEntity(ListRequestDTO dto) {
+        ReadingList list = new ReadingList();
+        list.setUserId(dto.getUserId());
+        list.setName(dto.getName());
+        list.setType(dto.getType());
+        list.setIsPrivate(dto.getIsPrivate());
+        return list;
+    }
+
+    public ListResponseDTO toResponseDTO(ReadingList list) {
+        ListResponseDTO dto = new ListResponseDTO();
+        dto.setId(list.getId());
+        dto.setUserId(list.getUserId());
+        dto.setName(list.getName());
+        dto.setType(list.getType());
+        dto.setIsPrivate(list.getIsPrivate());
+        dto.setCreatedAt(list.getCreatedAt());
+        dto.setItems(list.getItems().stream()
+                .map(this::toItemResponseDTO)
+                .collect(Collectors.toList()));
+        return dto;
+    }
+
+    public ReadingListItem toItemEntity(ItemRequestDTO dto) {
+        ReadingListItem item = new ReadingListItem();
+        item.setBookId(dto.getBookId());
+        item.setProgress(dto.getProgress());
+        return item;
+    }
+
+    public ItemResponseDTO toItemResponseDTO(ReadingListItem item) {
+        ItemResponseDTO dto = new ItemResponseDTO();
+        dto.setId(item.getId());
+        dto.setBookId(item.getBookId());
+        dto.setProgress(item.getProgress());
+        dto.setAddedAt(item.getAddedAt());
+        dto.setFinishedAt(item.getFinishedAt());
+        return dto;
+    }
 }
