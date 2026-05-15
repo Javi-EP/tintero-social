b971844683629238b4187dc9e264e629d05f874a
Eloy-Contreras <el.contrerasy@duocuc.cl>
2026-05-15 19:32:21 -0400

    creacion de clase GernePreference

---
+++ recommendation-service/src/main/java/cl/javiep/recommendationservice/Entity/GenrePreference.java
@@ -0,0 +1,26 @@
+package cl.javiep.recommendationservice.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "genre_preferences",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"userId", "genreId"}))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GenrePreference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long genreId;
+
+    private Double weight;
+}
