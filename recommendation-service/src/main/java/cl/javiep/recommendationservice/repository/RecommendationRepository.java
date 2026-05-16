2e82d27a1c6144a4c205b14fa5805bd52623a28b
Eloy-Contreras <el.contrerasy@duocuc.cl>
2026-05-16 07:37:18 -0400

    se arregla recommendatiorRepository

--- recommendation-service/src/main/java/cl/javiep/recommendationservice/repository/RecommendationRepository.java
+++ recommendation-service/src/main/java/cl/javiep/recommendationservice/repository/RecommendationRepository.java
@@ -4,7 +4,7 @@ import cl.javiep.recommendationservice.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface RecommendationRepository<Recommendation> extends JpaRepository<Recommendation, Long> {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
     List<Recommendation> findByUserIdAndDismissedFalse(Long userId);
     boolean existsByUserIdAndBookId(Long userId, Long bookId);
